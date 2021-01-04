b67c8151fd3a9c15df910167e1652e8b4b3f80c0
Thanmay47 <32269423+Thanmay47@users.noreply.github.com>
2021-01-05 01:13:54 +0530

    Add files via upload

---
+++ assets/js/my-file.js
@@ -0,0 +1,72 @@
+var player1 = document.getElementById("player0");
+var player2 = document.getElementById("player1");
+var player3 = document.getElementById("player2");
+var player4 = document.getElementById("player3");
+var player5 = document.getElementById("player4");
+var player6 = document.getElementById("player5");
+var player7 = document.getElementById("player6");
+var player8 = document.getElementById("player7");
+var player9 = document.getElementById("player8");
+
+player1.onclick = function()
+{
+    //first colummn stuff
+};
+
+player2.onclick = function()
+{
+    //first colummn stuff
+};
+
+player3.onclick = function()
+{
+    //first colummn stuff
+};
+
+player4.onclick = function()
+{
+    //first colummn stuff
+};
+
+player5.onclick = function()
+{
+    //first colummn stuff
+};
+
+player6.onclick = function()
+{
+    //first colummn stuff
+};
+
+player7.onclick = function()
+{
+    //first colummn stuff
+};
+
+player8.onclick = function()
+{
+    //first colummn stuff
+};
+
+player9.onclick = function()
+{
+    //first colummn stuff
+};
+
+var list_ten = new Array(10);//array to store the list of 10 top players retrieved froom db
+list_ten[0] = 1;//placeholder to be filled with a value retrieved from database
+list_ten[1] = 1;
+list_ten[2] = 1;
+list_ten[3] = 1;
+list_ten[4] = 1;
+list_ten[5] = 1;
+list_ten[6] = 1;
+list_ten[7] = 1;
+list_ten[8] = 1;
+list_ten[9] = 1;
+
+var findrrbtn = document.getElementById("findrr_btn2");
+findrrbtn.onclick = function()
+{
+
+};
