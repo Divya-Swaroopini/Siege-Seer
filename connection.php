b40ede8b16082fd2426f02e1dab0161361798f95
Divya Swaroopini Shridhar <swaroopini13@gmail.com>
2021-03-31 15:09:02 +0530

    lol I used the wrong db name. I should stop making commits before checking the little things :/

--- connection.php
+++ connection.php
@@ -2,6 +2,6 @@
     $host = "localhost";
     $username = "root";
     $password = "";
-    $db = "lfg_assistant";
+    $db = "seige_seer";
     $conn = new mysqli ($host, $username, $password, $db) or die("Can't connect to database :(");
 ?>
